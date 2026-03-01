792c39a87d416a3600fff53f4f1783138936363a
guntur.bramanda <guntur.bramanda@ifortepay.id>
2026-03-01 23:40:05 +0700

    Commit Guntur update

--- payment-core/build.gradle.kts
+++ payment-core/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
     id("org.jetbrains.kotlin.plugin.serialization")
@@ -14,9 +16,36 @@ kotlin {
 
     androidTarget()
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+//    iosX64()
+//    iosArm64()
+//    iosSimulatorArm64()
+
+
+    val paymentCoreXCFramework = XCFramework("PaymentCoreSDK")
+
+    iosX64 {
+        binaries.framework {
+            baseName = "PaymentCoreSDK"
+            isStatic = true
+            paymentCoreXCFramework.add(this)
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = "PaymentCoreSDK"
+            isStatic = true
+            paymentCoreXCFramework.add(this)
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = "PaymentCoreSDK"
+            isStatic = true
+            paymentCoreXCFramework.add(this)
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -52,4 +81,68 @@ android {
             targetCompatibility = JavaVersion.VERSION_17
         }
     }
+}
+tasks.register("buildSdk") {
+    group = "sdk"
+    description = "Build Payment SDK for Android and iOS"
+
+    dependsOn(
+        // Android
+        "compileDebugKotlinAndroid",
+        "compileReleaseKotlinAndroid",
+
+        // iOS
+        "compileKotlinIosArm64",
+        "compileKotlinIosX64",
+        "compileKotlinIosSimulatorArm64"
+    )
+}
+
+tasks.register("cleanBuildSdk") {
+    group = "sdk"
+    description = "Clean & build Payment SDK for Android and iOS"
+
+    dependsOn(
+        "clean",
+        "buildSdk"
+    )
+}
+tasks.register("assembleSdk") {
+    group = "sdk"
+    description = "Assemble Android AAR and iOS XCFramework"
+
+    dependsOn(
+        "assembleRelease",
+        "assemblePaymentCoreSDKReleaseXCFramework"
+    )
+}
+tasks.register<Copy>("exportIosXCFramework") {
+    group = "sdk"
+    description = "Export iOS XCFramework to dist/ios"
+
+    dependsOn("assemblePaymentCoreSDKReleaseXCFramework")
+
+    from(layout.buildDirectory.dir("XCFrameworks/release"))
+    into(layout.projectDirectory.dir("../dist/ios"))
+}
+
+tasks.register<Copy>("exportAndroidAar") {
+    group = "sdk"
+    description = "Export Android AAR to dist/android"
+
+    dependsOn("assembleRelease")
+
+    from(layout.buildDirectory.dir("outputs/aar"))
+    into(layout.projectDirectory.dir("../dist/android"))
+}
+
+tasks.register("exportSdk") {
+    group = "sdk"
+    description = "Build & export Android + iOS SDK"
+
+    dependsOn(
+        "assembleSdk",
+        "exportIosXCFramework",
+        "exportAndroidAar"
+    )
 }
